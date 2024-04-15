57188d1fcee39a13d16ebfd5a278c3639b549ff2
uhablog <uha.blog98@gmail.com>
2024-04-16 08:10:37 +0900

    スキーマとバリデーションの追加

--- ch02/orders/api/api.py
+++ ch02/orders/api/api.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 from uuid import UUID
 
-from starlette.response import Response
-from starlette import status
+# from starlette.response import Response
+# from starlette import status
+
+from fastapi import status
 
 from orders.app import app
+from orders.api.schemas import CreateOrderSchema
 
 # レスポンスで返す注文オブジェクトを定義
 orders = {
@@ -19,3 +22,31 @@ orders = {
         }
     ]
 }
+
+@app.get('/orders')
+def get_orders():
+    return {'orders': [orders]}
+
+@app.post('/orders', status_code=status.HTTP_201_CREATED)
+def create_order(order_details: CreateOrderSchema):
+    return orders
+
+@app.get('/orders/{order_id}')
+def get_order(order_id: UUID):
+    return orders
+
+@app.put('/orders/{order_id}')
+def update_order(order_id: UUID, order_details: CreateOrderSchema):
+    return orders
+
+@app.delete('/orders/{order_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_order(order_id: UUID):
+    return orders
+
+@app.post('/orders/{order_id}/cancel')
+def cancel_order(order_id: UUID):
+    return orders
+
+@app.post('/orders/{order_id}/pay')
+def pay_order(order_id: UUID):
+    return orders
