1418e293a57c9718fcb3cda475c1546c1617ef7f
uhablog <uha.blog98@gmail.com>
2024-04-16 12:55:25 +0900

    注文APIをFastAPIで作成
    - FastAPIはpydanticを使って、検証モデルの作成ができる
    - FastAPIは自動でSwaggerUIを生成する

--- ch02/orders/api/api.py
+++ ch02/orders/api/api.py
@@ -1,52 +1,95 @@
+import uuid
+
 from datetime import datetime
 from uuid import UUID
 
-# from starlette.response import Response
+from fastapi import status, HTTPException
+from starlette.responses import Response
 # from starlette import status
 
-from fastapi import status
-
 from orders.app import app
-from orders.api.schemas import CreateOrderSchema
+from orders.api.schemas import (
+    GetOrderSchema,
+    CreateOrderSchema,
+    GetOrdersSchema
+)
 
 # レスポンスで返す注文オブジェクトを定義
-orders = {
-    'id': 'ff0f1355-e821-4178-9567-550dec27a373',
-    'status': 'deliverd',
-    'created': datetime.now(),
-    'order': [
-        {
-            'product': 'cappucino',
-            'size': 'medium',
-            'quantity': 1
-        }
-    ]
-}
+ORDERS = []
 
-@app.get('/orders')
+@app.get('/orders', response_model=GetOrdersSchema)
 def get_orders():
-    return {'orders': [orders]}
+    return {'orders': ORDERS}
 
-@app.post('/orders', status_code=status.HTTP_201_CREATED)
+@app.post('/orders',
+          status_code=status.HTTP_201_CREATED,
+          response_model=GetOrderSchema)
 def create_order(order_details: CreateOrderSchema):
-    return orders
 
-@app.get('/orders/{order_id}')
+    # 注文データに必要な情報を付け加えて、リストに追加
+    order = order_details.dict()
+    order['id'] = uuid.uuid4()
+    order['created'] = datetime.now()
+    order['status'] = 'created'
+    ORDERS.append(order)
+    return order
+
+@app.get('/orders/{order_id}', response_model=GetOrderSchema)
 def get_order(order_id: UUID):
-    return orders
 
-@app.put('/orders/{order_id}')
+    # 注文リストからIDで検索して、返却する
+    for order in ORDERS:
+        if order['id'] == order_id:
+            return order
+    
+    # 注文が見つからない場合は404を返す
+    raise HTTPException(
+        status_code=404, detail=f'Order with ID {order_id} is not found'
+    )
+
+@app.put('/orders/{order_id}', response_model=GetOrderSchema)
 def update_order(order_id: UUID, order_details: CreateOrderSchema):
-    return orders
 
-@app.delete('/orders/{order_id}', status_code=status.HTTP_204_NO_CONTENT)
+    # IDが同じ注文情報を更新して、返却
+    for order in ORDERS:
+        if order['id'] == order_id:
+            order.update(order_details.dict())
+            return order
+    
+    raise HTTPException(
+        status_code=404, detail=f'Order with ID {order_id} is not found'
+    )
+
+@app.delete('/orders/{order_id}', 
+            status_code=status.HTTP_204_NO_CONTENT,
+            response_class=Response)
 def delete_order(order_id: UUID):
-    return orders
 
-@app.post('/orders/{order_id}/cancel')
+    for index, order in enumerate(ORDERS):
+        if order['id'] == order_id:
+            ORDERS.pop(index)
+            return Response(status_code=204)
+    
+    raise HTTPException(
+        status_code=404, detail=f'Order with ID {order_id} is not found'
+    )
+
+@app.post('/orders/{order_id}/cancel', response_model=GetOrderSchema)
 def cancel_order(order_id: UUID):
-    return orders
+    for order in ORDERS:
+        if order['id'] == order_id:
+            order['status'] = 'cancelled'
+            return order
+    raise HTTPException(
+        status_code=404, detail=f'Order with ID {order_id} is not found'
+    )
 
-@app.post('/orders/{order_id}/pay')
+@app.post('/orders/{order_id}/pay', response_model=GetOrderSchema)
 def pay_order(order_id: UUID):
-    return orders
+    for order in ORDERS:
+        if order['id'] == order_id:
+            order['status'] = 'progress'
+            return order
+    raise HTTPException(
+        status_code=404, detail=f'Order with ID {order_id} is not found'
+    )
