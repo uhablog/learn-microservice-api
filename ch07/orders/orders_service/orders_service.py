fa0f31b14ba5871005ad2ba7c1f72053c3715c81
uhablog <uha.blog98@gmail.com>
2024-04-25 17:45:20 +0900

    P183 モックサーバーの確認まで完了

---
+++ ch07/orders/orders_service/orders_service.py
@@ -0,0 +1,24 @@
+from repository.orders_repository import OrdersRepository
+
+class OrdersService:
+    def __init__(self, orders_repository):
+        self.repository = orders_repository
+
+    def place_order(self, item):
+        pass
+
+    def get_order(self, item):
+        pass
+
+    def update_order(self, item):
+        pass
+
+    def list_order(self, item):
+        pass
+
+    def pay_order(self, item):
+        pass
+
+    def cancel_order(self, item):
+        pass
+
