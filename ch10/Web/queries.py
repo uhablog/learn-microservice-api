a7186a163c7b38439646d302c3f97588d1659ec6
uhablog <uha.blog98@gmail.com>
2024-05-08 22:40:06 +0900

    resolverでinputを受け取る

--- ch10/Web/queries.py
+++ ch10/Web/queries.py
@@ -1,5 +1,12 @@
+from itertools import islice
 from ariadne import QueryType
 from Web.data import ingredients, products
+
+def get_page(items, items_per_page, page):
+    page = page - 1
+    start = items_per_page * page if page > 0 else page
+    stop = start + items_per_page
+    return list(islice(items, start, stop))
 
 query = QueryType()
 
@@ -10,3 +17,29 @@ def resolve_all_ingredients(*_):
 @query.field('allProducts')
 def resolve_all_products(*_):
     return products
+
+@query.field('products')
+def resolve_products(*_, input=None):
+
+    filtered = [product for product in products]
+    if input is None:
+        return filtered
+
+    if input.get('minPrice') is not None:
+        filtered = [
+            product for product in filtered
+            if product['price'] >= input['minPrice']
+        ]
+
+    if input.get('maxPrice') is not None:
+        filtered = [
+            product for product in filtered
+            if product['price'] <= input['maxPrice']
+        ]
+
+    filtered.sort(
+        key=lambda product: product.get(input['sortBy'], 0),
+        reverse=input['sort'] == 'DESCENDING'
+    )
+
+    return get_page(filtered, input['resultsPerPage'], input['page'])
